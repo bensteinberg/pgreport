f3c17bc88c1ee7a86af05b1cfea6c328776fef43
Ben Steinberg <ben@potswift.org>
2024-10-07 10:01:14 -0400

    Correct pygit2 submodule invocations

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,5 +6,5 @@ def pytest_configure(config):
     Ensure submodule is up-to-date.
     """
     repo = pygit2.Repository('.')
-    repo.init_submodules()
-    repo.update_submodules()
+    repo.submodules.init()
+    repo.submodules.update()
